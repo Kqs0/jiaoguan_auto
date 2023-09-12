3f0c7f7801e88ffe5c6b007517667998c17a9b90
Kqs0 <144645466+Kqs0@users.noreply.github.com>
2023-09-12 16:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 
 if __name__ == '__main__':
     # 根据result目录下的文件生成allure测试报告html文件
-    os.system("allure generate ./result -o ./reports --clean")
+    os.system("allure generate ./reports -o ./html --clean")
